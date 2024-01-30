c8f59716bde5419c48dc9a82c4bb303b634f3d9e
Ken San Angelo <ken@kensanangelo.com>
2024-01-30 15:17:32 -0500

    Cleans up api code to read more like an actual api response

--- src/services/api.ts
+++ src/services/api.ts
@@ -18,26 +18,28 @@ const ApiService = {
 	},
 
 	async postTask(task: TaskForm): Promise<Task> {
-		const result = await Promise.resolve({
-			status: 'success',
-			data: null,
-		});
-
-		if (result.status !== 'success') {
-			throw new Error('Error posting task');
-		}
-
 		// NOTE: In real life, we'd get the ID from the server
 		// Here we're just generating a random number to replicate that
 		// In rare circumstances, this may cause a duplicate key warning
 		// but that would not occur in the real app
 		const newId = Math.floor(Math.random() * (9999 - 6)) + 6;
 
-		return {
-			...task,
-			id: newId,
-			status: 'Not Started' as Status,
-		};
+		const result = await Promise.resolve({
+			status: 'success',
+			data: {
+				task: {
+					...task,
+					id: newId,
+					status: 'Not Started' as Status,
+				},
+			},
+		});
+
+		if (result.status !== 'success') {
+			throw new Error('Error posting task');
+		}
+
+		return result.data.task;
 	},
 };
 
